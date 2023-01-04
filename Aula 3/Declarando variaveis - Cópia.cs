41759af531e069b39d9108704ac92423c7aa6527
mariojamisson10 <121949385+mariojamisson10@users.noreply.github.com>
2023-01-04 18:02:08 -0300

    Aula 3 - Variáveis Númericas

---
+++ Aula 3/Declarando variaveis - Cópia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string []args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+        int idade;
+        idade = 27;
+        //Console.WriteLine(idade); Isso vai imprimir só a idade. Posso adicionar mais coisas
+        Console.WriteLine("Eu tenho " + idade + " anos de idade");
+
+        Console.WriteLine("////////////////////////////////////////////////");
+
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+        double salario = 1200.00;
+        Console.WriteLine(salario);
+    }
+}
