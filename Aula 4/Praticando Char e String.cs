6459b500f3dd496d54e01f8c49498c18941e1638
mariojamisson10 <121949385+mariojamisson10@users.noreply.github.com>
2023-01-04 18:03:14 -0300

    Aula 4 - Variáveis de texto

---
+++ Aula 4/Praticando Char e String.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letra = 'a';
+        Console.WriteLine(letra);
+        char valor = (char)65;                    
+        Console.WriteLine(valor);
+        valor = (char)(valor + 1);          
+        Console.WriteLine(valor);
+    }
+}
