15b20efe181c2fc5f7860a7695dda35ad9404339
mariojamisson10 <121949385+mariojamisson10@users.noreply.github.com>
2023-01-04 18:05:41 -0300

    Aula 6 - Controle de fluxo com laços de repetição

---
+++ Aula 6/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+
+    static void Main(string[] args)
+    {
+        int contador = 0;
+        while (contador <= 10)
+        {
+            Console.WriteLine(contador);
+            contador = contador += 1;
+        }
+        Console.ReadLine();
+    }
+}
